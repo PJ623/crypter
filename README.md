e64a31d01c9cda1c394bd5b4a69bc0d79c15717c
PJ623 <jardiolin.paolo@gmail.com>
2020-02-14 13:25:19 -0800

    This is the first commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+Crypter
+===
+
+
+Crypter is a Node.js app that encrypts and decrypts files that are saved in the app's "collection" folder.
+
+
+To decrypt files, simply run the app. The encrypt files, close the app either by closting the terminal with `CTRL-C` or by submitting a specific close phrase (phrases such as "quit", "stop", "q", or "gg") into the Node.js terminal.
---
+++ crypter.js
@@ -0,0 +1,87 @@
+(function Crypter() {
+
+    const fs = require("fs");
+
+    const crypto = require("crypto");
+    const algorithm = "aes-256-cbc";
+    const key = "passwordpasswordpasswordpassword";
+    const iv = crypto.randomBytes(16);
+
+    const readline = require("readline");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    function decrypt(encrypted) {
+        let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), Buffer.from(encrypted.iv, "hex"));
+        let decrypted = decipher.update(Buffer.from(encrypted.encryptedData, "hex"));
+        decrypted = Buffer.concat([decrypted, decipher.final()]);
+        return decrypted;
+    }
+
+    function encrypt(buffer) {
+        let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), Buffer.from(iv));
+        let encrypted = cipher.update(buffer);
+        encrypted = Buffer.concat([encrypted, cipher.final()]);
+        return { iv: Buffer.from(iv).toString("hex"), encryptedData: encrypted.toString("hex") }; //{iv: iv.toString("hex"), data: encrypted.toString("hex")} // returns a buffer
+    }
+
+    function decryptAll() {
+        let collection = fs.readdirSync("collection");
+        for (let i = 0; i < collection.length; i++) {
+            let entry = fs.readFileSync(__dirname + "/collection/" + collection[i].toString());
+            try {
+                if (JSON.parse(entry).iv) {
+                    fs.writeFileSync(__dirname + "/collection/" + collection[i].toString(), decrypt(JSON.parse(entry)));
+                    console.log(collection[i].toString() + " is now decrypted.");
+                }
+            } catch (e) {
+                console.log(collection[i].toString() + " did not need to be decrypted.");
+            }
+        }
+    }
+
+    function encryptAll() {
+        let collection = fs.readdirSync("collection");
+        for (let i = 0; i < collection.length; i++) {
+            let entry = fs.readFileSync(__dirname + "/collection/" + collection[i].toString());
+            fs.writeFileSync(__dirname + "/collection/" + collection[i].toString(), JSON.stringify(encrypt(entry)));
+            console.log(collection[i].toString() + " is now encrypted.");
+        }
+    }
+
+    function close() {
+        encryptAll();
+        console.log("Goodbye!");
+        rl.close();
+    }
+
+    (function open() {
+        console.log("Hello!");
+        decryptAll();
+    })();
+
+    rl.on("line", function (line) {
+        switch (line.toLowerCase()) {
+            case "bye":
+            case "exit":
+            case "gg":
+            case "q":
+            case "quit":
+            case "abort":
+            case "stop":
+            case "close":
+            case "end":
+                close();
+                break;
+            default:
+                console.log("Sorry, I don't understand what '" + line + "' means.");
+                break;
+        }
+    });
+
+    rl.on("SIGINT", function () {
+        close();
+    });
+})();
